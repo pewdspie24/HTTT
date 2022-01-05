f22c2fba865544a334c3cba6f105d9d2103ed0ae
Quang <quangtd.24it@gmail.com>
2022-01-06 00:26:29 +0700

    update

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
+import json
 from underthesea import word_tokenize as vn_token_uts
-from vncorenlp import VnCoreNLP
 
-def vn_token_vnc(text):
-    annotator = VnCoreNLP("HTTT_Prj\VnCoreNLP\VnCoreNLP-1.1.1.jar", annotators="wseg,pos,ner,parse", max_heap_size='-Xmx2g') 
-    word_segmented_text = annotator.tokenize(text)[0]
-    final = []
-    for word in word_segmented_text:
-        word = word.replace('_', ' ')
-        final.append(word)
+def get_token(text):
+    final = ""
+    for word in text:
+        final += word
+        final += " "
     return final
 
-sentence = "mọi người đều thích điều này"
+def get_from_JSON(text, list):
+    # if text == 
+    return 0
 
-abc = vn_token_uts(sentence)
-print(abc)
+if __name__ == "__main__":
+    # English loading
+    with open("eng_dict\data.json", 'r', encoding="utf8") as j:
+        vi_eng_dict = json.loads(j.read())
+    with open("eng_dict\pronouns.json", 'r', encoding="utf8") as j:
+        pronoun = json.loads(j.read())
+    with open("eng_dict\singular_noun.json", 'r', encoding="utf8") as j:
+        singular_noun = json.loads(j.read())
+    with open("eng_dict\\verb.json", 'r', encoding="utf8") as j:
+        verbs = json.loads(j.read())
+    with open("eng_dict\words_dictionary.json", 'r', encoding="utf8") as j:
+        eng_dict = json.loads(j.read())
+
+    # Vietnamese loading
+    for idx, i in enumerate(vi_eng_dict):
+        try:
+            a = i["word"]
+            # TODO: trans1 without trans0 and "Như"
+        except Exception as e:
+            print(vi_eng_dict[idx])
+            print(idx)
+            
+    # Process
+    # while True:
+    #     sequence = input()
+    #     tokens = vn_token_uts(sequence)
+
+
+    # sentence = "mọi người đều thích điều này"
+    # abc = get_token(sentence)
+    # print(abc)
